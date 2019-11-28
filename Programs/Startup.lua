ed60b1436befd31da9cecc4f0071ecd5e0f60bae
MattTheWorm <Matttheworm@gmail.com>
2019-11-28 13:51:08 -0500

    ceated startup

---
+++ Programs/Startup.lua
@@ -0,0 +1,41 @@
+--[[\\\\\\\\\\\\\\\\\\\\\
+
+Vars
+
+\\\\\\\\\\\\\\\\\\\\\\\--]]
+
+--[[\\\\\\\\\\\\\\\\\\\\\
+
+Func
+
+\\\\\\\\\\\\\\\\\\\\\\\--]]
+
+local function update()
+	testPrograms = {"HelloWorld.lua"}
+	mainPrograms = {"Startup.lua"}
+	gitPath = "https://raw.githubusercontent.com/MattTheWorm/ComputerCraft-Fun/master/Programs/"
+	
+	for i = 1, #mainPrograms do --Main Programs
+		shell.run("delete " .. mainPrograms[i])
+		shell.run("wget " .. gitPath .. mainPrograms[i])
+	
+	end
+	
+	for i = 1, #testPrograms do --Test Programs
+		shell.run("delete " .. testPrograms[i])
+		shell.run("wget " .. gitPath .. "/Tests/" .. testPrograms[i])
+	
+	end
+	
+	print("Update complete.")
+
+end
+
+--[[\\\\\\\\\\\\\\\\\\\\\
+
+Main
+
+\\\\\\\\\\\\\\\\\\\\\\\--]]
+
+print("Starting..")
+update()
