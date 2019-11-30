44d10598924a45bb8cf7bf687c8984027763ca69
MattTheWorm <Matttheworm@gmail.com>
2019-11-30 15:17:59 -0500

    v1

--- Programs/BinaryDoorController.lua
+++ Programs/BinaryDoorController.lua
@@ -13,10 +13,12 @@ Notes:
 -- Vars
 --
 local pistonArray = {}
+local platePositions = {}
 local maxNumber = 10 --Max number of pistons 
 local startPoint --Point in which pistons start to split from
 local x1, x2
-local sleepTime, waitTime = 0.07, 4
+local sleepTime, waitTime = 0.10, 5
+
 
 --
 -- Func
@@ -57,6 +59,7 @@ end
 --
 for i = 1, maxNumber do
   pistonArray[i] = 0
+  platePositions[2^(i - 1)] = i 
   --print("Piston position "  .. i .. " ready.")
 
 end
@@ -64,79 +67,80 @@ end
 --
 -- Main
 --
-while true do
-	os.pullEvent()
+for y = 1, 5 do
+	local event = os.pullEvent()
 	
-	if redstone.testBundledInput("right", 1) then
-		print("White pressed!!")
-		
-	else
-		print("White not pressed.")
+	print("Event found: ", event)
+	if rs.getBundledInput("top") ~= 0 then
+		print("Pressure plate pressed. Value: ", rs.getBundledInput("top"), "Position: ", platePositions[rs.getBundledInput("top")])
+		startPoint = platePositions[rs.getBundledInput("top")]
+		x1, x2 = startPoint, startPoint
+		sleep(0.3)
+		pistonArray[startPoint] = 1
+		print(x1, x2)
+		outputToPistons(stateToBinary(pistonArray))
+		sleep(sleepTime)
+		for i = 1, maxNumber do
+		  
+		  
+		  if x1 ~= 1 then
+			x1 = x1 - 1
+			pistonArray[x1] = 1
+
+		  end
+
+		  if x2 ~= maxNumber then
+			x2 = x2 + 1
+			pistonArray[x2] = 1
+
+		  end
+		  
+		  print(x1, x2)
+		  outputToPistons(stateToBinary(pistonArray))
+		  sleep(sleepTime)
+
+		  if x2 == maxNumber and x1 == 1 then
+			break
+			
+		  end
+		  
+		end
+
+		sleep(waitTime)
+		pistonArray[x1], pistonArray[x2] = 0
+		print(x1, x2)
+		outputToPistons(stateToBinary(pistonArray))
+		sleep(sleepTime)
+		for i = 1, maxNumber do
+		  
+		  
+		  if x1 ~= startPoint then
+			x1 = x1 + 1
+			pistonArray[x1] = 0
+
+		  end
+
+		  if x2 ~= startPoint then
+			x2 = x2 - 1
+			pistonArray[x2] = 0
+
+		  end
+		  
+		  print(x1, x2)
+		  outputToPistons(stateToBinary(pistonArray))
+		  sleep(sleepTime)
+
+		  if x2 == startPoint and x1 == startPoint then
+			break
+			
+		  end
+		  
+		end
+
+		print("Complete")
+		--return
 		
 	end
 
 end
-x1, x2 = startPoint, startPoint
-sleep(waitTime)
-pistonArray[startPoint] = 1
-print(x1, x2)
-outputToPistons(stateToBinary(pistonArray))
-sleep(sleepTime)
-for i = 1, maxNumber do
-  
-  
-  if x1 ~= 1 then
-    x1 = x1 - 1
-    pistonArray[x1] = 1
 
-  end
-
-  if x2 ~= maxNumber then
-    x2 = x2 + 1
-    pistonArray[x2] = 1
-
-  end
-  
-  print(x1, x2)
-  outputToPistons(stateToBinary(pistonArray))
-  sleep(sleepTime)
-
-  if x2 == maxNumber and x1 == 1 then
-    break
-	
-  end
-  
-end
-
-sleep(waitTime)
-pistonArray[x1], pistonArray[x2] = 0
-print(x1, x2)
-outputToPistons(stateToBinary(pistonArray))
-sleep(sleepTime)
-for i = 1, maxNumber do
-  
-  
-  if x1 ~= startPoint then
-    x1 = x1 + 1
-    pistonArray[x1] = 0
-
-  end
-
-  if x2 ~= startPoint then
-    x2 = x2 - 1
-    pistonArray[x2] = 0
-
-  end
-  
-  print(x1, x2)
-  outputToPistons(stateToBinary(pistonArray))
-  sleep(sleepTime)
-
-  if x2 == startPoint and x1 == startPoint then
-    break
-	
-  end
-  
-end
-
-print("Complete")
