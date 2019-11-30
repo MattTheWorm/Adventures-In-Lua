d1db9e7dff66d119cd18649fb24b6d8a7450f75c
MattTheWorm <Matttheworm@gmail.com>
2019-11-29 21:45:16 -0500

    Update Programs/BinaryDoorController.lua

--- Programs/BinaryDoorController.lua
+++ Programs/BinaryDoorController.lua
@@ -4,7 +4,7 @@ Notes:
 	Such that if you have 8 outputs --> 00000000 each piston is a 0, but it's just direct binary
 	If the rightmost piston is on.. 00000001 The number value is 1
 	If the leftmost value is on.. 10000000 The number value for this set is 128
-
+ *Pistons 
 
 
 
@@ -14,9 +14,10 @@ print("Now looping")
 for x = 1, 5 do
 	for i = 0, 10 do --Test
 		rs.setBundledOutput("back", 2^i)
-		sleep(0.25)
+		sleep(0.05)
 		rs.setBundledOutput("back", 2^i + 2^(i+1))
-		sleep(0.25)
+		sleep(0.05)
+		rs.setBundledOutput("back", 2^i + 2^(i+2))
 		
 	end
 end
